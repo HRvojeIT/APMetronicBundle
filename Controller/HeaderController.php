c1d351caf961a464b18de68cf99d8985638adb94
HRvojeIT <angelo@angelopellegrinon.it>
2016-09-14 12:53:12 +0200

    Continuazione test per passaggio variabili nel render dei menu.

--- Controller/HeaderController.php
+++ Controller/HeaderController.php
@@ -8,32 +8,37 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HeaderController extends Controller
 {
-	
-    public function _showNotificationsAction()
+
+    public function _showNotificationsAction($params)
     {
 		// Verifica se è stato inserito il servizio apmetronic.header, in caso contrario
 		// nasconde le notifiche
+		print_r($params);
+		echo "AAAA";
+		print_r($request->query);
+		exit();
 		if($this->container->has('apmetronic.header')) {
 			$notificationArray = $this->get('apmetronic.header')->_getNotifications();
-			return 
-				$this->render('APMetronicBundle:metronic/pezzi/header:notification_dropdown.html.twig', 
+			array_push($notificationArray, 'activetasks' => 2,)
+			return
+				$this->render('APMetronicBundle:metronic/pezzi/header:notification_dropdown.html.twig',
 				$notificationArray
 			);
 		} else
 			return new Response();
     }
-	
+
 	public function _showTasksAction()
     {
 		// Verifica se è stato inserito il servizio apmetronic.header, in caso contrario
 		// nasconde i tasks
 		if($this->container->has('apmetronic.header')) {
 			$tasksArray = $this->get('apmetronic.header')->_getTasks();
-			return 
-				$this->render('APMetronicBundle:metronic/pezzi/header:todo_dropdown.html.twig', 
+			return
+				$this->render('APMetronicBundle:metronic/pezzi/header:todo_dropdown.html.twig',
 				$tasksArray);
 		} else
 			return new Response();
     }
-	
+
 }
